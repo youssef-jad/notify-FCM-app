e8ce7e02cc81a2563d48d206fe7e111311ff2de4
Youssef Gad <youssef-jad@users.noreply.github.com>
2021-12-27 19:49:02 +0000

    Apply fixes from StyleCI

--- src/notify-app/app/FCM/Interfaces/SendNotificationsContract.php
+++ src/notify-app/app/FCM/Interfaces/SendNotificationsContract.php
@@ -1,11 +1,8 @@
 <?php
 
-
 namespace App\Fcm\Interfaces;
-
 
 interface SendNotificationsContract
 {
-
     public function send($data);
 }
--- src/notify-app/app/FCM/Traits/FCMClient.php
+++ src/notify-app/app/FCM/Traits/FCMClient.php
@@ -3,14 +3,10 @@
 namespace App\Fcm\Traits;
 
 use App\Device;
-use App\User;
 use GuzzleHttp\Client;
-use Illuminate\Support\Facades\Route;
-
 
 trait FCMClient
 {
-
     public $client;
     public $sound = 'default';
     public $priority = 'high';
@@ -23,32 +19,33 @@ trait FCMClient
 
     public function __construct()
     {
-        $this->fcmKey = env('FIREBASE_FCM_KEY' , 'dont hard coded here ');
+        $this->fcmKey = env('FIREBASE_FCM_KEY', 'dont hard coded here ');
         $this->headers = [
-            'Authorization' => ['key=' . $this->fcmKey ],
-            'Content-Type'=> 'application/json'
+            'Authorization' => ['key='.$this->fcmKey],
+            'Content-Type'  => 'application/json',
         ];
         $this->client = new Client();
     }
 
-
     /**
      * @param $data
+     *
      * @return array
      */
     private function setFields($data)
     {
         return [
-            'registration_ids' =>  $this->getTokens($data),
-            'priority' => $this->priority,
+            'registration_ids'  => $this->getTokens($data),
+            'priority'          => $this->priority,
             'content_available' => $this->content_available,
-            'data' => ["title" => $data->title, "body" => $data->body, 'sound' => $this->sound],
-            'notification' => ["title" => $data->title, "body" => $data->body, 'sound' => $this->sound],
+            'data'              => ['title' => $data->title, 'body' => $data->body, 'sound' => $this->sound],
+            'notification'      => ['title' => $data->title, 'body' => $data->body, 'sound' => $this->sound],
         ];
     }
 
     /**
      * @param $jsonData
+     *
      * @return FCMClient
      */
     private function request($jsonData)
@@ -57,58 +54,61 @@ trait FCMClient
             $this->fcm_send_url,
             [
                 'headers' => $this->headers,
-                'json' => $jsonData
+                'json'    => $jsonData,
             ]
         );
     }
 
-
     /**
      * @param \Psr\Http\Message\ResponseInterface $request
+     *
      * @return array
      */
     private function response(\Psr\Http\Message\ResponseInterface $request)
     {
         $decodedResult = json_decode($this->request->getBody()->getContents());
+
         return [
-            'multicast_id' => $decodedResult->multicast_id,
-            'success' => $decodedResult->success,
-            'failure' => $decodedResult->failure,
+            'multicast_id'  => $decodedResult->multicast_id,
+            'success'       => $decodedResult->success,
+            'failure'       => $decodedResult->failure,
             'canonical_ids' => $decodedResult->canonical_ids,
             'response_code' => $request->getStatusCode(),
-            'results' => $decodedResult->results,
-        ];;
+            'results'       => $decodedResult->results,
+        ];
     }
 
     /**
      * @param $data
      * @param $fields
+     *
      * @return mixed
      */
     protected function setFieldsData($data, $fields)
     {
         if (count($data) > 0) {
-            $fields[ 'data' ] = $data;
+            $fields['data'] = $data;
         }
+
         return $fields;
     }
 
     /**
      * @param $data
+     *
      * @return array
      */
     private function getTokens($data)
     {
 
-        // 
+        //
         if ($data->user == null) {
             // set Tokens to Bulk
             $this->devices = new Device();
+
             return $this->devices->groupedKeys()->pluck('device_key')->toArray();
         }
 
         return $data->user->devices->pluck('device_key')->toArray();
     }
-
-
 }
